4ccfe7559b0f658b0bb9e5441498d7e73abc53f2
L1smanity <lismanity@gmail.com>
2024-12-22 20:05:35 +0100

    changes

--- src/app/services/[servicesSlug]/page.tsx
+++ src/app/services/[servicesSlug]/page.tsx
@@ -2,23 +2,28 @@
 import React from "react";
 import servicesData from "../../../../public/services.json"; // Adjust the import path as necessary
 import { useParams } from "next/navigation";
+import Hero from "@/components/homepage/Hero";
 
 const SingleService = () => {
-  const { servicesSlug } = useParams(); // Use useParams to get the slug
+  const { servicesSlug } = useParams();
 
-  // Find the service based on the slug
   const service = servicesData.services.find((s) => s.slug === servicesSlug);
 
   if (!service) {
-    return <div>Service not found</div>; // Handle case where service is not found
+    return <div>Service not found</div>;
   }
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">{service.title}</h1>
-      <p>{service.description}</p>
+      <Hero />
+
+      <h1 className="text-3xl px-[4%] py-28 font-bold">{service.title}</h1>
+      {service.description && (
+        <div>
+          <p>{service.description}</p>
+        </div>
+      )}
       <div>{service.content}</div>
-      {/* Add more service details as needed */}
     </div>
   );
 };
