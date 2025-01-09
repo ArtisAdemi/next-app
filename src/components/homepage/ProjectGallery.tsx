216d3dc313026d1118af3184a23702aa739d211b
Florian Berisha <florian.fani@hotmail.com>
2025-01-09 01:03:27 +0100

    Project Gallery Redone, also responsive

--- src/components/homepage/ProjectGallery.tsx
+++ src/components/homepage/ProjectGallery.tsx
@@ -32,6 +32,10 @@ const ProjectGallery = forwardRef<HTMLDivElement>((_, ref) => {
     },
     // Add more projects as needed
   ];
+
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState<number | 0>(
+    0
+  );
   const [sliderPositions, setSliderPositions] = useState<number[]>(
     Array(projects.length).fill(50)
   );
@@ -69,47 +73,80 @@ const ProjectGallery = forwardRef<HTMLDivElement>((_, ref) => {
   };
 
   return (
-    <div ref={ref} className="px-[4%] pb-28">
-      <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
+    <div ref={ref} className="px-[4%] lg:px-[12%] py-28">
+      <h1 className="text-4xl md:text-5xl font-extrabold mb-8 text-center text-gray-800">
         Before and After Comparisons
       </h1>
-      <div className="w-full relative flex flex-col lg:flex-row space-y-10 md:space-y-0 space-x-4">
+
+      {/* Display Selected Project with Comparison */}
+      {selectedProjectIndex !== null && (
+        <div className="mt-10 w-full relative">
+          <div
+            className="relative w-full  mx-auto aspect-[70/60] lg:aspect-[20/10] overflow-hidden select-none rounded-xl shadow-lg"
+            onMouseMove={(e) => handleMouseMove(e, selectedProjectIndex)}
+            onTouchMove={(e) => handleTouchMove(e, selectedProjectIndex)}
+            onMouseDown={() => setIsDraggingIndex(selectedProjectIndex)}
+            onTouchStart={() => setIsDraggingIndex(selectedProjectIndex)}
+            onMouseUp={() => setIsDraggingIndex(null)}
+            onTouchEnd={() => setIsDraggingIndex(null)}
+          >
+            <Image
+              alt={`Hero image of Project ${selectedProjectIndex + 1}`}
+              fill
+              quality={100}
+              priority
+              src={projects[selectedProjectIndex].heroImage}
+              className="object-cover rounded-xl"
+            />
+            <div
+              className="absolute top-0 left-0 right-0 w-full aspect-[70/60] lg:aspect-[20/10] m-auto overflow-hidden select-none"
+              style={{
+                clipPath: `inset(0 ${
+                  100 - sliderPositions[selectedProjectIndex]
+                }% 0 0)`,
+              }}
+            >
+              <Image
+                alt={`Background image of Project ${selectedProjectIndex + 1}`}
+                fill
+                priority
+                quality={100}
+                src={projects[selectedProjectIndex].backgroundImage}
+                className="object-cover rounded-xl"
+              />
+            </div>
+            <div
+              className="absolute top-0 bottom-0 w-1 bg-white cursor-ew-resize"
+              style={{
+                left: `calc(${sliderPositions[selectedProjectIndex]}% - 1px)`,
+              }}
+            >
+              <div className="bg-white absolute rounded-full h-3 w-3 -left-1 top-[calc(50%-5px)] shadow-lg" />
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="w-full mt-12 flex flex-wrap gap-y-6 justify-between">
+        {/* Display Thumbnails */}
         {projects.map((project, index) => (
           <div
             key={project.id}
-            className="project-comparison flex-shrink-0 min-w-[300px]"
+            className="project-comparison relative w-[150px] xl:w-[200px] h-[150px] xl:h-[200px] cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out"
+            onClick={() => setSelectedProjectIndex(index)}
           >
-            <div
-              className="relative w-full aspect-[70/45] overflow-hidden select-none"
-              onMouseMove={(e) => handleMouseMove(e, index)}
-              onTouchMove={(e) => handleTouchMove(e, index)}
-              onMouseDown={() => setIsDraggingIndex(index)}
-              onTouchStart={() => setIsDraggingIndex(index)}
-              onMouseUp={() => setIsDraggingIndex(null)}
-              onTouchEnd={() => setIsDraggingIndex(null)}
-            >
-              <Image alt="" fill priority src={project.heroImage} />
-              <div
-                className="absolute top-0 left-0 right-0 w-full aspect-[70/45] m-auto overflow-hidden select-none"
-                style={{
-                  clipPath: `inset(0 ${100 - sliderPositions[index]}% 0 0)`,
-                }}
-              >
-                <Image fill priority alt="" src={project.backgroundImage} />
-              </div>
-              <div
-                className="absolute top-0 bottom-0 w-1 bg-white cursor-ew-resize"
-                style={{
-                  left: `calc(${sliderPositions[index]}% - 1px)`,
-                }}
-              >
-                <div className="bg-white absolute rounded-full h-3 w-3 -left-1 top-[calc(50%-5px)]" />
-              </div>
-            </div>
+            <Image
+              alt={`Project ${project.id}`}
+              className="object-cover rounded-lg shadow-md"
+              src={project.heroImage}
+              quality={100}
+              fill
+            />
           </div>
         ))}
       </div>
     </div>
   );
 });
+
 export default ProjectGallery;
